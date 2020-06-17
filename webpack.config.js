0a375f1830bebc9d10b1d92e1f1a7d642d95dd82
andres <aebb001@gmail.com>
2020-06-17 09:30:47 -0400

    Hicimos el home.js agregamos la carpeta templates header.js

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+/****************NOTA*/
+
+/*Hacerse un cursito de webpack para entender esta vaina*/ 
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin([
+      {
+        inject: true,
+        template: './public/index.html',
+        filename: './index.html',
+      }
+    ]),
+  ]
+}
