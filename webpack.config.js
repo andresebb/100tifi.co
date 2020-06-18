ed3dd29ef9cbdacf2c53890d47363a6f81f267c1
andres <aebb001@gmail.com>
2020-06-18 10:42:30 -0400

    Anadiendo estilos

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -32,6 +35,11 @@ module.exports = {
         template: './public/index.html',
         filename: './index.html',
       }
-    )
-  ]
-}
+    ),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './src/styles/styles.css', to: ''},
+      ],
+    }),
+  ],
+};
